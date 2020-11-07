ebebb341560bee8b82489ab6ce6d6b539f6360c7
Paras Agrawal <paras@piktorlabs.com>
2020-11-08 03:46:48 +0530

    Add header of child tweets section

--- src/components/ChildTweets.vue
+++ src/components/ChildTweets.vue
@@ -6,6 +6,11 @@
         alt="Profile Picture"
         class="childtweets-profilePic"
       />
+      <div class="childtweets-header-name">Paras Agrawal</div>
+      <div class="green-dot"></div>
+      <div class="childtweets-header-room">Room: 102</div>
+      <div class="childtweets-header-time">Oct 1 - Oct 12</div>
+      <div class="childtweets-header-create">Create a task</div>
     </section>
   </main>
 </template>
@@ -19,9 +24,41 @@ export default {
 <style lang="scss" scoped>
 @import "../utils/commonStyles/flex.scss";
 
+.childtweets {
+  width: 100%;
+}
+.childtweets-header {
+  @include flex-row-v-cen;
+  border-bottom: 1px solid #dbdbdb;
+  padding: 10px 20px;
+  position: relative;
+}
 .childtweets-profilePic {
   width: 20px;
   height: auto;
   border-radius: 50%;
+}
+.childtweets-header-name {
+  padding-left: 10px;
+}
+.green-dot {
+  width: 10px;
+  height: 10px;
+  border-radius: 50%;
+  background: #29cb41;
+  margin-left: 10px;
+}
+.childtweets-header-room {
+  padding-left: 40px;
+}
+.childtweets-header-time {
+  padding-left: 60px;
+}
+.childtweets-header-create {
+  position: absolute;
+  right: 30px;
+  background: #f7f6f3;
+  padding: 5px 15px;
+  border-radius: 10px;
 }
 </style>
