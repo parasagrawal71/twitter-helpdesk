4d1e79089161d6b6d277e44c49c1039d185fccc8
Paras Agrawal <paras@piktorlabs.com>
2020-11-08 19:29:07 +0530

    Update request_token to our backend endpoint

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -25,22 +25,36 @@ export default {
       //   .then((response) => console.log("response: ", response))
       //   .catch((e) => console.log("Error: ", e));
 
+      // const config = {
+      //   method: "post",
+      //   url: "https://api.twitter.com/oauth/request_token",
+      //   headers: {
+      //     Authorization: `OAuth oauth_consumer_key="ywkvzrkLoWlJBDu1yYvBOgywg",oauth_signature_method="HMAC-SHA1",oauth_timestamp="1604841931",oauth_nonce="K7ny27JTpKVsTgdyLeDfmQQWVLELj2zAK5BslRsqyw",oauth_version="1.0",oauth_callback="${encodeURIComponent(
+      //       window.location.origin
+      //     )}",oauth_signature="jV1z4bGB4y1GOXLkTcKossodT8A%3D"`,
+      //   },
+      // };
+
+      // axios(config)
+      //   .then(function(response) {
+      //     console.log(JSON.stringify(response.data));
+      //   })
+      //   .catch(function(error) {
+      //     console.log(error);
+      //   });
+
       const config = {
-        method: "post",
-        url: "https://api.twitter.com/oauth/request_token",
-        headers: {
-          Authorization: `OAuth oauth_consumer_key="ywkvzrkLoWlJBDu1yYvBOgywg",oauth_signature_method="HMAC-SHA1",oauth_timestamp="1604841931",oauth_nonce="K7ny27JTpKVsTgdyLeDfmQQWVLELj2zAK5BslRsqyw",oauth_version="1.0",oauth_callback="${encodeURIComponent(
-            window.location.origin
-          )}",oauth_signature="jV1z4bGB4y1GOXLkTcKossodT8A%3D"`,
-        },
+        method: "get",
+        url:
+          "https://twitter-helpdesk--server.herokuapp.com/api/v1/auth/request_token",
       };
 
       axios(config)
-        .then(function(response) {
-          console.log(JSON.stringify(response.data));
+        .then((response) => {
+          console.log("response", response);
         })
-        .catch(function(error) {
-          console.log(error);
+        .catch((error) => {
+          console.log("Error: ", error);
         });
     },
 
