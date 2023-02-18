78e62b619816f9cb6eda8bae95fbb3318129b761
Paras Agrawal <parasagrawal71@gmail.com>
2023-02-18 13:34:49 +0530

    Change socket url

--- src/pages/Conversations.vue
+++ src/pages/Conversations.vue
@@ -69,7 +69,7 @@ export default {
 
     const ORIGIN = window.location.origin;
     const HOST = ORIGIN?.includes("https")
-      ? "ws://twitter-socket-server.up.railway.app/"
+      ? "wss://twitter-socket-server.up.railway.app/"
       : "ws://127.0.0.1:6002";
     this.client = new w3cwebsocket(
       `${HOST}?oauth_token=${userData?.oauth_token}&oauth_token_secret=${userData?.oauth_token_secret}&screen_name=${userData?.screen_name}`
